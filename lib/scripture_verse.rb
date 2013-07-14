9ea1246407d017ddc997d87a3ada2cd9b8669c31
Gagan Awhad <gagan.awhad@desiringgod.org>
2013-07-14 15:32:48 -0500

    ScriptureBook will get initialized implicitly within ScriptureChapter

--- lib/scripture_verse.rb
+++ lib/scripture_verse.rb
@@ -1,7 +1,6 @@
 class ScriptureVerse
   def initialize book_number, chapter_number, verse_number
-    @scripture_book = ScriptureBook.new(book_number)
-    @scripture_chapter = ScriptureChapter.new(chapter_number)
+    @scripture_chapter = ScriptureChapter.new(book_number, chapter_number)
     raise ArgumentError, 'verse number cannot be converted to an integer' unless verse_number.respond_to?(:to_i)
     raise 'verse number does not exist' if (1..@scripture_chapter.number_of_verses).cover? verse_number.to_i
     @verse_number = verse_number if verse_number.to_i
