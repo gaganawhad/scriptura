285c5a59a2f32712f3bc84d076a0d1343b03f0b8
Gagan Awhad <gagan.awhad@desiringgod.org>
2013-07-14 14:19:26 -0500

    Correction

--- spec/scripture_book_spec.rb
+++ spec/scripture_book_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ScriptureBook do
   describe '#initialize' do 
-    it 'raises an error if the arugment cannot be converted to a string' do
+    it 'raises an error if the arugment cannot be converted to an integer' do
       lambda { ScriptureBook.new(:a1) }.should raise_error(ArgumentError)
     end
 
